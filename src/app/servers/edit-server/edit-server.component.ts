1487d2d63273a9526b82fb5fe62dd190ddef15e0
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-28 14:09:15 +0300

    implement passing query params through components

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,18 +12,27 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService,
     private route: ActivatedRoute) { }
 
   ngOnInit() {
-    console.log(this.route.snapshot.queryParams);
-    console.log(this.route.snapshot.fragment);
-    this.route.queryParams.subscribe();
-    this.route.fragment.subscribe();
-    this.server = this.serversService.getServer(1);
+    // console.log(this.route.snapshot);
+    // console.log(this.route.snapshot.fragment);
+    this.route.queryParams.subscribe(
+      (params: Params) => {
+        console.log(params)
+        this.allowEdit = params.allowEdit === '1' ? true : false;
+      }
+    );
+    // this.route.fragment.subscribe();
+    this.server = this.serversService.getServer(+this.route.snapshot.params.id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(params.id);
+    });
   }
 
   onUpdateServer() {
